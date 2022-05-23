75340078795f10ded1465003d9196add4a0dc54a
Nicky <fatemesahraee2000@gmail.com>
2022-05-24 01:21:47 +0430

    some bugs fix in forms and button

--- components/Partnership/PartnershipFormContainer.js
+++ components/Partnership/PartnershipFormContainer.js
@@ -4,13 +4,13 @@ import PartnershipForm from "./PartnershipForm";
 export default function PartnershipFormContainer() {
     return (
         <div>
-            <div className = {styles.formContainer + " hidden min-h-[145vh] md:flex items-end justify-center pb-24"}>
+            <div className = {styles.formContainer + " hidden min-h-[145vh] lg:flex items-end justify-center pb-24"}>
                 <div className = {" flex flex-row justify-between items-center gap-8"}>
                     <PartnershipForm/>
                 </div>
             </div>
 
-            <div className = {"md:hidden pb-24 "}>
+            <div className = {"lg:hidden pb-24 "}>
                 <img src={"/common/papercut-mobile.png"} className={"object-none"}/>
                 <div className = {" flex flex-col justify-between items-center gap-8 bg-shades-10 pb-4"} id={"formMobile"}>
                     <PartnershipForm/>
--- components/Startup/StartupFormContainer.js
+++ components/Startup/StartupFormContainer.js
@@ -5,13 +5,13 @@ import PartnershipForm from "../Partnership/PartnershipForm";
 export default function StartupFormContainer() {
     return (
         <div>
-            <div className = {styles.formContainer + " hidden min-h-[135vh] md:flex items-end justify-center pb-24"}>
+            <div className = {styles.formContainer + " hidden min-h-[135vh] lg:flex items-end justify-center pb-24"}>
                 <div className = {" flex flex-row justify-between items-center gap-8"}>
                     <StartupForm/>
                 </div>
             </div>
 
-            <div className = {"md:hidden pb-24"}>
+            <div className = {"lg:hidden pb-24"}>
                 <img src={"/common/papercut-mobile.png"} className={"object-none"}/>
                 <div className = {" flex flex-col justify-between items-center gap-8 bg-shades-10 pb-4"} id={"formMobile"}>
                     <StartupForm/>
