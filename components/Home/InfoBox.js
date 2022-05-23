bf66a978a0bf0d9957a4a7a52e15fa047fdc252c
Nicky <fatemesahraee2000@gmail.com>
2022-05-23 17:00:53 +0430

    update countups

--- components/Home/InfoBox.js
+++ components/Home/InfoBox.js
@@ -5,8 +5,8 @@ export default function InfoBox() {
     return (
         <div className = {"flex flex-col-reverse gap-10 lg:flex-row items-center justify-center lg:justify-around mt-32 lg:mt-56"} id = {"InfoBox"}>
             <InfoItem prefix = {"بیش از"} number = {20} suffix = {"سال سابقه"} body = {"تجربه کاری"}/>
-            <InfoItem prefix = {""} number = {3} suffix = {""} body = {"مرکز اداری تجاری بین المللی"}/>
-            <InfoItem prefix = {"+"} number = {5} suffix = {"میلیارد دلار"} body = {"دارایی های تحت مدیریت"}/>
+            <InfoItem prefix = {""} number = {10} suffix = {""} body = {"تفاهم نامه‌های همکاری"}/>
+            <InfoItem prefix = {""} number = {13} suffix = {"پورتفولیو"} body = {"استارتاپی"}/>
         </div>
     )
 }
