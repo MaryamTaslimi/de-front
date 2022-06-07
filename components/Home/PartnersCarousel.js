8419240e0358f4b80ed815ae2b7e6f27a665e740
Nicky <fatemesahraee2000@gmail.com>
2022-06-07 17:10:02 +0430

    change a bug in PartnersCarousel.js

--- components/Home/PartnersCarousel.js
+++ components/Home/PartnersCarousel.js
@@ -2,8 +2,8 @@ import styles from "../../styles/Home.module.css";
 
 export default function PartnersCarousel() {
     const items = [
-        {name: "شبکه اینترنتی اکوایران", src: '/home/logo-p1.png'},
-        {name: "انجمن سرمایه گذاران خطرپذیر", src: '/home/logo-p2.png'},
+        {name: "شبکه اینترنتی اکوایران", src: '/home/logo-p1.png', className: "w-20 md:w-24"},
+        {name: "انجمن سرمایه گذاران خطرپذیر", src: '/home/logo-p2.png', className: "w-20 md:w-24"},
         {name: "پایگاه خبری دنیای بورس", src: '/home/logo-p3.png'},
         {name: "اتاق بازرگانی ایران", src: '/home/logo-p4.png'},
         {name: "خانه نوآوری و فناوری سلیم", src: '/home/logo-p5.png'},
@@ -14,13 +14,13 @@ export default function PartnersCarousel() {
     ]
     return (
 
-        <div className = {" flex flex-col justify-center items-center z-10"}>
+        <div className = {styles.gradientBg + " " + styles.hiddenXScrollBar + " flex flex-col justify-center items-center z-10 w-full overflow-x-scroll"}>
             <div
-                className = {styles.gradientBg + " " + styles.hiddenXScrollBar + " w-[95vw] xl:w-2/3 overflow-x-scroll px-12 py-6"}>
-                <div className = {"flex flex-row gap-24 whitespace-nowrap px-6"}>
+                className = {" py-6 w-full"}>
+                <div className = {"flex flex-row gap-20 whitespace-nowrap"}>
                     {items.map((item) => (
                         <div key = {item.name}
-                             className = {"w-20 md:w-24 flex flex-col justify-end items-center gap-4 inline-block"}>
+                             className = {"w-fit flex flex-col justify-end items-center gap-4 inline-block px-4"}>
                             <img src = {item.src}
                                  className = {item.className + " md:grayscale md:opacity-25 duration-700 ease-out hover:opacity-100 hover:filter-none"}/>
                             <p className = {" text-white font-light text-base"}>{item.name}</p>
@@ -31,4 +31,3 @@ export default function PartnersCarousel() {
         </div>
     );
 }
-
