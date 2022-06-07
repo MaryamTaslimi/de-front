e644aab1e52a845f685147fafd4eeecbd9d409a2
Nicky <fatemesahraee2000@gmail.com>
2022-06-08 00:14:47 +0430

    adding autoPlay to PartnersCarousel.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
-const withPlugins = require("next-compose-plugins");
-const withCSS = require("@zeit/next-css");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-const config = {
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
-      use: {
-        loader: "url-loader",
-        options: {
-          limit: 8192,
-          publicPath: "/_next/static/",
-          outputPath: "static/",
-          name: "[name].[ext]"
-        }
-      }
-    });
-    return config;
-  }
-};
-
-module.exports = withPlugins([withCSS], config);
+module.exports = nextConfig
