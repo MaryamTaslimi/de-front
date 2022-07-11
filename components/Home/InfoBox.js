0b9450193ba42aac9afbef6e032987e99685a79d
Nicky <fatemesahraee2000@gmail.com>
2022-07-11 17:49:41 +0430

    updates done

--- components/Home/InfoBox.js
+++ components/Home/InfoBox.js
@@ -6,8 +6,8 @@ export default function InfoBox() {
         <div className = {"flex flex-col-reverse gap-10 lg:flex-row items-center justify-center lg:justify-around mt-32 lg:mt-56"} id = {"InfoBox"}>
             <InfoItem prefix = {""} number = {20} suffix = {"+"} body = {"تجربه کاری"}/>
             <InfoItem prefix = {""} number = {20} suffix = {"+"} body = {"شریک تجاری"}/>
-            <InfoItem prefix = {""} number = {20} suffix = {"+"} body = {"سرمایه گذار"}/>
-            <InfoItem prefix = {""} number = {20} suffix = {"+"} body = {"سرمایه پذیر"}/>
+            <InfoItem prefix = {""} number = {9} suffix = {"+"} body = {"سرمایه گذار"}/>
+            <InfoItem prefix = {""} number = {33} suffix = {"+"} body = {"سرمایه پذیر"}/>
         </div>
     )
 }
